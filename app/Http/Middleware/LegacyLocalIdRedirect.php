0d628e440be7f89693ea672959fea0238fb2a1b3
ildyria <beviguier@gmail.com>
2026-02-27 22:43:01 +0100

    also add guard

--- app/Http/Middleware/LegacyLocalIdRedirect.php
+++ app/Http/Middleware/LegacyLocalIdRedirect.php
@@ -10,6 +10,8 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use function Safe\preg_match;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class LegacyLocalIdRedirect
@@ -24,9 +26,13 @@ class LegacyLocalIdRedirect
 	 */
 	public function handle(Request $request, \Closure $next)
 	{
+		// Also skip if the column does not exists.
+		// That way we don't crash on database which had this column removed.
 		$album_id = $request->route('albumId');
-
-		if ($album_id !== null && strlen($album_id) === 14 && is_numeric($album_id)) {
+		if ($album_id !== null &&
+			preg_match('/^\d{14}$/', (string) $album_id) === 1 &&
+			Schema::hasColumn('base_albums', 'legacy_id')
+		) {
 			$album = DB::table('base_albums')->where('legacy_id', '=', $album_id)->first();
 			if ($album === null) {
 				throw new NotFoundHttpException();
